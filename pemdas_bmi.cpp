fbeabaefd60cf1edaf79522ae4a24215945f54cb
abiiiaja <kalindraaraya@gmail.com>
2026-03-10 20:08:45 +0700

    Menambahkan fungsi untuk menghitung BMI

--- pemdas_bmi.cpp
+++ pemdas_bmi.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
 
-float weight, height, bmi;
+float weight, height, bmii;
 
 void input(){
+    cout << endl;
     cout << "--- Selamat Datang Mahasiswa Kelas B 2025 ---" << endl;
     cout << "Masukkan berat badan (kg): ";
     cin >> weight;
-    cout << "Masukkan tinggi badan (m): ";
-    cin >> height; 
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> height;
+    cout << endl; 
+}
+
+float calculatorBMI(float b, float t){
+    return b/(t*t);
 }
