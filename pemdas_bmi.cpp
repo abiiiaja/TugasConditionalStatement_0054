2f6e805ce11367c97aff287e42a683036cdfab09
abiiiaja <kalindraaraya@gmail.com>
2026-03-10 20:18:28 +0700

    Merapikan beberapa typo

--- pemdas_bmi.cpp
+++ pemdas_bmi.cpp
@@ -5,7 +5,7 @@ float weight, height, bmii;
 
 void input(){
     cout << endl;
-    cout << "--- Selamat Datang Mahasiswa Kelas B 2025 ---" << endl;
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
     cout << "Masukkan berat badan (kg): ";
     cin >> weight;
     cout << "Masukkan tinggi badan (meter): ";
