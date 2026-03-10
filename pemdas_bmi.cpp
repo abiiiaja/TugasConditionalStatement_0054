c9f45f5c20527c8ece4de9a9087b9b0f4f3f7307
abiiiaja <kalindraaraya@gmail.com>
2026-03-10 19:44:46 +0700

    Menambahkan prosedur input: berat badan

--- pemdas_bmi.cpp
+++ pemdas_bmi.cpp
@@ -2,3 +2,11 @@
 using namespace std;
 
 float weight, height, bmi;
+
+void input(){
+    cout << "--- Selamat Datang Mahasiswa Kelas B 2025 ---" << endl;
+    cout << "Masukkan berat badan (kg): ";
+    cin >> weight;
+    cout << "Masukkan tinggi badan (m): ";
+    cin >> height;
+}
